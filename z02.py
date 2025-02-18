66e1336380d7faf50813c25ce6bd475fa2a2ca30
nikfer123 <nikfer9@mail.ru>
2025-02-18 14:53:06 +0000

    Занятие 2

--- z02.py
+++ z02.py
@@ -1,19 +1,24 @@
 import timeit
 import random
 
+cnt = 0
+
 def fun(arr, target):
     left, right = 0, len(arr) - 1  
     while left <= right:
         mid = left + (right - left) // 2  
         if arr[mid] == target:
+            cnt += 1
             return mid  
         elif arr[mid] < target: 
             left = mid + 1
+            cnt += 1
             return left  
         else: 
             right = mid - 1
+            cnt += 1
             return right
-    return 
+    return cnt
 
 start = timeit.default_timer()
 print('The start time is :', start)
